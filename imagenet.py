8db7716e68d1a4659ccd25566dccc7f8a3daa082
JiazeWang <jiazewang@csu.edu.cn>
2019-08-14 23:04:07 +0800

    1

--- imagenet.py
+++ imagenet.py
@@ -101,6 +101,7 @@ def get_imagenet_iter_torch(type, image_dir, batch_size, num_threads, device_id,
 
 
 if __name__ == '__main__':
+    """
     train_loader = get_imagenet_iter_dali(type='train', image_dir='/mnt/SSD/imagenet', batch_size=256,
                                           num_threads=4, crop=224, device_id=4, num_gpus=1)
     print('start iterate')
@@ -111,8 +112,8 @@ if __name__ == '__main__':
     end = time.time()
     print('end iterate')
     print('dali iterate time: %fs' % (end - start))
-
-    train_loader = get_imagenet_iter_torch(type='train', image_dir='/userhome/data/imagenet', batch_size=256,
+    """
+    train_loader = get_imagenet_iter_torch(type='train', image_dir='/mnt/SSD/imagenet', batch_size=256,
                                            num_threads=4, crop=224, device_id=4, num_gpus=1)
     print('start iterate')
     start = time.time()
